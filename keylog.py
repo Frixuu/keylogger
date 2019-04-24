6947fa318c43aba1c25f42407c9c02807a16d59d
Frixu <frixu@frixu.me>
2019-04-24 09:50:33 +0200

    Better handle alt, shift, space and backspace

--- keylog.py
+++ keylog.py
@@ -4,18 +4,30 @@ from tkinter import messagebox, Tk
 import keyboard
 import os
 
-buffer_length = 20
-buffer = " " * buffer_length
-triggers = ["tr1", "w"]
+# Customize your trigger words and popup contents here
+triggers = ["forbidden", "555-02", "55502", "Smith"]
+popup_title = "Title"
+popup_body = "You should not do that!"
 
-def onKeyPress(event):
+max_buffer_length = 50
+buffer = ""
+
+def hook(event : keyboard.KeyboardEvent):
   global buffer
-  buffer = buffer[-buffer_length+1:] + event.name
-  print(buffer)
+  key = event.name
+  if key == "space":
+    key = " "
+  elif key == "backspace":
+    buffer = buffer[:-1]
+    return
+  elif key.endswith("shift") or key.startswith("alt"):
+    return
+  buffer = buffer[-max_buffer_length+1:] + key
+  # print(buffer)
   for word in triggers:
     if buffer.endswith(word):
       Tk().withdraw()
-      messagebox.showinfo("Title", "Contents")
+      messagebox.showinfo(popup_title, popup_body)
 
-keyboard.on_press(onKeyPress)
+keyboard.on_press(hook)
 keyboard.wait()
